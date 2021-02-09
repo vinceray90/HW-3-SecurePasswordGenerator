c09c95f9b20f576cdf7d5e2ba78ca17f939ae7f4
Vin Rodriguez <vince.ray90@gmail.com>
2021-02-08 23:19:47 -0600

    refined the javascript to ensure the passwords meet criteria every time

--- script.js
+++ script.js
@@ -14,59 +14,90 @@ function writePassword() {
 //pulled function generatePassword
 // created variables and with correlating prompts
 function generatePassword() {
+  //using PROMPT so the user is prompted to write the number of characters needed
  var passwordLength = prompt("Choose password between 8 -  128 characters."); 
  //used "if" statement to ensure password meets length criteria
  if( passwordLength  <8 || passwordLength >128) {
      alert("Password must be within 8 and 128 characters.");
      location.reload();
+     //using ELSE statement after confirming the user meets the length criteria
+     //using CONFIRM statement to determine if the following criteria is required for the password
  } else {
- var passwordLowercase = prompt("Does the password require lowercase characters? yes or no.");
- var passwordUppercase = prompt("Does the password require uppercase characters? yes or no.");
- var passwordNumeric = prompt("Does the password require numeric characters? yes or no.");
- var passwordSpecial = prompt("Does the password require special characters? yes or no.");
+ var passwordLowercase = confirm("Does the password require lowercase characters?");
+ var passwordUppercase = confirm("Does the password require uppercase characters?");
+ var passwordNumeric = confirm("Does the password require numeric characters?");
+ var passwordSpecial = confirm("Does the password require special characters?");
 
- //these are the options for the criteria
+ //variables associated with STRINGS of characters that meet said criteria
  var lowercase = "abcdefghijklmnopqrstuvwxyz"
  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
  var numeric = "0123456789"
  var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
  var fullstringPassword = ""
  }
- //create if statments for the prompts 
- // calls strings from criteria variables if "yes"
- if (passwordLowercase === "yes") {
+
+ //create IF statments to add criteria if TRUE to the fullstringPassword
+ // calls strings from criteria variables 
+ if (passwordLowercase) {
    fullstringPassword += lowercase
  }
- if (passwordUppercase === "yes") {
+ if (passwordUppercase) {
    fullstringPassword += uppercase
  }
- if (passwordNumeric === "yes"){
+ if (passwordNumeric){
    fullstringPassword += numeric
  }
- if (passwordSpecial === "yes"){
+ if (passwordSpecial){
    fullstringPassword += special
  }
- if (fullstringPassword === ""){
+ //ALERT in the event that the user provides no criteria
+ if (fullstringPassword == ""){
    alert("Please provide at least one criteria");
    location.reload();
  }
 
- //I need to generate a password with the fullstring accepting all criteria\
- // use math to randomize the password
- var generatedSecurePass = ""
- var i;
- for (var i = 0; i < passwordLength; i++) {
-  generatedSecurePass += fullstringPassword.charAt(Math.floor(Math.random() * fullstringPassword.length));
- } 
- return generatedSecurePass
+ // DO statement to test the passord until it meets the criteria once 
+ do {
+  var generatedSecurePass = ""
 
+  for (var i = 0; i < passwordLength; i++) {
+   generatedSecurePass += fullstringPassword.charAt(Math.floor(Math.random() * fullstringPassword.length));
+  }
+
+ console.log(generatedSecurePass)
+
+  var hasLower = false;
+  var hasUpper = false;
+  var hasNumeric = false;
+  var hasSpecial = false;
+  
+  //using the FOR loop and IF/ELSE IF statements to ensure that at least one of each requested criteria is met in the password
+  for (var i = 0; i < generatedSecurePass.length; i++) {
+   var char = generatedSecurePass[i];
+ 
+   if (lowercase.includes(char)) {
+     hasLower = true;
+   } else if (uppercase.includes(char)) {
+     hasUpper = true;
+   } else if (numeric.includes(char)) {
+     hasNumeric = true;
+   } else if (special.includes(char)) {
+     hasSpecial = true;
+   } 
+ 
+  }
 }
+//WHILE loop to determine if it NEEDS the criteria and if it HAS the criteria
+   while (
+    (passwordLowercase && !hasLower) ||
+    (passwordUppercase && !hasUpper) ||
+    (passwordNumeric && !hasNumeric) ||
+    (passwordSpecial && !hasSpecial)
+  ) 
 
-//create if statements to 
-
-//create prompts to choose length between 8 - 128 characters
-// prompt to determine character types such as lowercase, uppercase, numeric, and/or special characters
-// 
+  return generatedSecurePass
+ 
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
